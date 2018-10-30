0dd8c54fd819ddcdae404a741056eb2eacebb381
RIAD AISSAR SANTOS CHARRUF <A16016280@inet.uady.mx>
2018-10-30 17:45:29 -0600

    Solucion de errores y eliminaicon  de car´peta con nombre erroneo

--- funciones/base_de_datos/proceso_eliminar_cuenta.php
+++ funciones/base_de_datos/proceso_eliminar_cuenta.php
@@ -12,7 +12,7 @@
     $query2 = "DELETE from usuarios WHERE id_usuario='$id'";
     $resultado2 = $conexion->query($query2);
     if($resultado2){
-        header("Location:../../inicio.php");
+        header("Location:../../sistemas/sistema_login/logout.php");
         echo "Si se elimino";
     }else{
         header("Location:../../paginas/paginas_usuarios/informacion_perfil.php");
